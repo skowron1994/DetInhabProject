b1a7a830b196e86e93e6aa12d9ae3e9203fa1859
skowron <skowronek.kamil.1994@gmail.com>
2017-12-17 22:07:43 +0100

    asd

--- app/src/main/java/com/detectinhabitants/detinhab/LoginActivity.java
+++ app/src/main/java/com/detectinhabitants/detinhab/LoginActivity.java
@@ -61,6 +61,5 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
     }
 
--- app/src/main/java/com/detectinhabitants/detinhab/SettingsActivity.java
+++ app/src/main/java/com/detectinhabitants/detinhab/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.detectinhabitants.detinhab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Button btnUser = findViewById(R.id.btnInhManag);
+
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsActivity.this, AddHabitantActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
